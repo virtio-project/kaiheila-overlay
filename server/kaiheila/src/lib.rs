515cde1fd7d0a746df4bd4baee3845a428a5ffd0
lightsing <light.tsing@gmail.com>
2022-05-19 21:22:40 +0800

    impl multiplexer

--- server/kaiheila/src/lib.rs
+++ server/kaiheila/src/lib.rs
@@ -1,14 +1,17 @@
 mod message;
 
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
-use futures_util::StreamExt;
+use crate::message::Message;
+use futures_util::{SinkExt, StreamExt};
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::{mpsc, oneshot, Mutex};
 
 const CLIENT_ID: &str = "15943749139034";
 const DEFAULT_ENDPOINT: &str = "ws://127.0.0.1:5988/?url=";
 const TOKEN_URL: &str = "https://www.kaiheila.cn/api/oauth2/token";
-
-type WsStream = actix_codec::Framed<awc::BoxedSocket, actix_http::ws::Codec>;
 
 #[derive(Debug)]
 pub struct ClientBuilder {
@@ -23,15 +26,24 @@ pub struct ClientBuilder {
 pub enum ClientBuildError {
     #[error("incomplete client builder")]
     MissingField,
-    #[error("awc ws client error: {0}")]
-    WsClientError(#[from] awc::error::WsClientError),
-    #[error("ws protocol error: {0}")]
-    WsProtocolError(#[from] actix_http::ws::ProtocolError),
+    #[error("ws client error: {0}")]
+    WsClientError(#[from] tokio_tungstenite::tungstenite::Error),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ClientError {
+    #[error("message could not be sent")]
+    SenderError(Message),
+    #[error("message could not be retrieved")]
+    RecvError(#[from] tokio::sync::oneshot::error::RecvError),
+    #[error("ws client error: {0}")]
+    WsClientError(#[from] tokio_tungstenite::tungstenite::Error),
+    #[error("deserialization error: {0}")]
+    DeserializationError(#[from] serde_json::Error),
 }
 
 pub struct Client {
-    client: awc::Client,
-    stream: WsStream,
+    tx: mpsc::UnboundedSender<(Message, oneshot::Sender<Result<Message, ClientError>>)>,
 }
 
 impl Default for ClientBuilder {
@@ -75,20 +87,66 @@ impl ClientBuilder {
             self.channel_id.ok_or(ClientBuildError::MissingField)?
         );
         let url = format!("{}{}", self.endpoint, urlencoding::encode(url.as_str()));
-        let client = awc::Client::new();
-        let (res, mut ws) = client
-            .ws(url)
-            .origin("https://streamkit.kaiheila.cn")
-            .protocols(["ws_streamkit"])
-            .connect()
-            .await?;
-        debug!("{:?}", res);
-        let _ = ws.next().await.unwrap()?;
 
-        Ok(Client {
-            client,
-            stream: ws
-        })
+        let (tx, mut rx) =
+            mpsc::unbounded_channel::<(Message, oneshot::Sender<Result<Message, ClientError>>)>();
+
+        let (ws_stream, _) = tokio_tungstenite::connect_async(url).await?;
+        let (mut ws_tx, mut ws_rx) = ws_stream.split();
+
+        let handlers = Arc::new(Mutex::new(HashMap::new()));
+
+        let _handlers = handlers.clone();
+        // reading message and send to ws
+        tokio::spawn(async move {
+            while let Some((msg, handler)) = rx.recv().await {
+                {
+                    let mut guard = _handlers.lock().await;
+                    guard.insert(msg.id, handler);
+                    // we want to drop the guard as soon as possible
+                }
+
+                let packed = tokio_tungstenite::tungstenite::Message::Text(
+                    serde_json::to_string(&msg).unwrap(),
+                );
+                if let Err(e) = ws_tx.send(packed).await {
+                    let mut guard = _handlers.lock().await;
+                    let handler = guard.remove(&msg.id).unwrap();
+                    handler.send(Err(e.into())).ok();
+                }
+            }
+        });
+
+        // receiving message and passing it to handler
+        tokio::spawn(async move {
+            while let Some(ret) = ws_rx.next().await {
+                let _handlers = handlers.clone();
+                tokio::spawn(async move {
+                    let ret = ret
+                        .and_then(|m| m.into_text())
+                        .map_err(ClientError::from)
+                        .and_then(|s| {
+                            serde_json::from_str::<Message>(s.as_str()).map_err(ClientError::from)
+                        });
+                    match ret {
+                        Ok(msg) => {
+                            let mut guard = _handlers.lock().await;
+                            match guard.remove(&msg.id) {
+                                None => error!("unknown message: {:?}", msg),
+                                Some(handler) => {
+                                    if let Err(msg) = handler.send(Ok(msg)) {
+                                        error!("handler unexpectedly close, message cannot be sent: {:?}", msg)
+                                    }
+                                }
+                            }
+                        }
+                        Err(e) => error!("error receiving message: {:?}", e),
+                    }
+                });
+            }
+        });
+
+        Ok(Client { tx })
     }
 }
 
@@ -96,5 +154,12 @@ impl Client {
     pub fn new_builder() -> ClientBuilder {
         ClientBuilder::default()
     }
-}
 
+    pub async fn send_message(&self, message: Message) -> Result<Message, ClientError> {
+        let (tx, rx) = oneshot::channel();
+        self.tx
+            .send((message, tx))
+            .map_err(|mpsc::error::SendError((msg, _))| ClientError::SenderError(msg))?;
+        rx.await?
+    }
+}
--- server/kaiheila/src/message.rs
+++ server/kaiheila/src/message.rs
@@ -1,10 +1,21 @@
 use std::collections::HashMap;
 
-use serde::{Serialize, Deserialize};
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, thiserror::Error)]
+pub enum MessageError {
+    #[error("unsupported message")]
+    UnsupportedMessage,
+    #[error("deserialization failed: {0}")]
+    JsonError(#[from] serde_json::Error),
+    #[error("request resources not found")]
+    NotFound,
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-struct Message {
-    id: u32,
+pub struct Message {
+    pub(crate) id: u32,
     #[serde(skip_serializing_if = "Option::is_none")]
     args: Option<HashMap<String, serde_json::Value>>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -37,4 +48,119 @@ pub enum Event {
     MessageCreate,
     MessageUpdate,
     MessageDelete,
+}
+
+#[derive(Default, Clone, Debug)]
+pub struct SubscribeMessageBuilder {
+    args: HashMap<String, serde_json::Value>,
+    event: Option<Event>,
+}
+
+impl Message {
+    pub fn get_args_string<K: AsRef<str>>(&self, k: K) -> Result<String, MessageError> {
+        Message::get_string_inner(&self.args, k)
+    }
+
+    pub fn get_data_string<K: AsRef<str>>(&self, k: K) -> Result<String, MessageError> {
+        Message::get_string_inner(&self.data, k)
+    }
+
+    fn get_string_inner<K: AsRef<str>>(
+        map: &Option<HashMap<String, serde_json::Value>>,
+        k: K,
+    ) -> Result<String, MessageError> {
+        match map {
+            None => Err(MessageError::NotFound),
+            Some(m) => m
+                .get(k.as_ref())
+                .ok_or(MessageError::NotFound)
+                .and_then(|v| v.as_str().ok_or(MessageError::NotFound).map(str::to_string)),
+        }
+    }
+
+    pub fn authorize_req<C: AsRef<str>>(client_id: C) -> Self {
+        let mut rng = rand::thread_rng();
+        Message {
+            id: rng.gen_range(1000000..9999999),
+            args: Some(HashMap::from_iter([
+                (
+                    "client_id".to_string(),
+                    serde_json::Value::String(client_id.as_ref().to_string()),
+                ),
+                (
+                    "scopes".to_string(),
+                    serde_json::Value::Array(vec![
+                        serde_json::Value::String("rpc".to_string()),
+                        serde_json::Value::String("get_guild_info".to_string()),
+                    ]),
+                ),
+                (
+                    "prompt".to_string(),
+                    serde_json::Value::String("none".to_string()),
+                ),
+            ])),
+            data: None,
+            cmd: Cmd::Authorize,
+            event: None,
+        }
+    }
+
+    pub fn authenticate_req<C: AsRef<str>>(client_id: C, token: String) -> Self {
+        let mut rng = rand::thread_rng();
+        Message {
+            id: rng.gen_range(1000000..9999999),
+            args: Some(HashMap::from_iter([
+                (
+                    "client_id".to_string(),
+                    serde_json::Value::String(client_id.as_ref().to_string()),
+                ),
+                ("token".to_string(), serde_json::Value::String(token)),
+            ])),
+            data: None,
+            cmd: Cmd::Authenticate,
+            event: None,
+        }
+    }
+
+    // {"id":1216596,"args":{"channel_id":"1714016194916588","guild_id":"1561035437838649"},"cmd":"subscribe","evt":"audio_channel_user_change"}
+    // {"id":9599219,"args":{"channel_id":"1714016194916588"},"cmd":"subscribe","evt":"audio_channel_user_talk"}
+    // {"id":7153765,"args":{"channel_id":"1714016194916588"},"cmd":"subscribe","evt":"audio_channel_mic_headerset_status"}
+    pub fn subscribe_builder() -> SubscribeMessageBuilder {
+        SubscribeMessageBuilder::default()
+    }
+}
+
+impl SubscribeMessageBuilder {
+    pub fn channel_id<C: AsRef<str>>(mut self, channel_id: C) -> Self {
+        self.args.insert(
+            "channel_id".to_string(),
+            serde_json::Value::String(channel_id.as_ref().to_string()),
+        );
+        self
+    }
+
+    pub fn guild_id<G: AsRef<str>>(mut self, guild_id: G) -> Self {
+        self.args.insert(
+            "guild_id".to_string(),
+            serde_json::Value::String(guild_id.as_ref().to_string()),
+        );
+        self
+    }
+
+    pub fn event(mut self, event: Event) -> Self {
+        self.event = Some(event);
+        self
+    }
+
+    pub fn build(self) -> Message {
+        let mut rng = rand::thread_rng();
+        let Self { args, event } = self;
+        Message {
+            id: rng.gen_range(1000000..9999999),
+            args: Some(args),
+            data: None,
+            cmd: Cmd::Subscribe,
+            event,
+        }
+    }
 }
